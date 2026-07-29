9f74f361c97b180a7cd9e116046c2061a76190db
syoukoukaikaihatu <narikoh.shiroma@gmail.com>
2026-07-30 08:42:53 +0900

    Add files via upload

---
+++ shops.js
@@ -0,0 +1,237 @@
+/*
+============================================================
+ YEG祭2026 出店者情報
+ 今後の店名・提供品目・アルコール・写真名はこのファイルだけ編集
+============================================================
+
+【GitHubでの修正方法】
+1. exhibitors.js を開く
+2. 右上の鉛筆マークを押す
+3. ダブルクォーテーション " " の内側だけ修正
+4. Commit changes を押す
+
+【注意】
+・idの数字、{ }、カンマは消さないでください。
+・写真だけ変更するときは同じ画像ファイル名で上書きしてください。
+・imageを空欄にすると写真を表示しません。
+============================================================
+*/
+
+window.SHOPS = [
+  // 1番
+  {
+    id: 1,
+    name: "アイドルブース",
+    description: "物販ブースブッパン",
+    alcohol: "無し",
+    image: "exhibitor-01.png"
+  },
+
+  // 2番
+  {
+    id: 2,
+    name: "ゆきいもゆりちゃんカフェ",
+    description: "冷やしキュウリヒ",
+    alcohol: "無し",
+    image: "exhibitor-02.png"
+  },
+
+  // 3番
+  {
+    id: 3,
+    name: "個別教育Can",
+    description: "フランクフルト",
+    alcohol: "無し",
+    image: "exhibitor-03.png"
+  },
+
+  // 4番
+  {
+    id: 4,
+    name: "YEGブース",
+    description: "",
+    alcohol: "有り",
+    image: "exhibitor-04.jpg"
+  },
+
+  // 5番
+  {
+    id: 5,
+    name: "和香",
+    description: "焼きそば、大人のかき氷オトナゴオリ",
+    alcohol: "有り",
+    image: "exhibitor-05.png"
+  },
+
+  // 6番
+  {
+    id: 6,
+    name: "居酒屋祇園",
+    description: "焼き鳥",
+    alcohol: "有り",
+    image: "exhibitor-06.png"
+  },
+
+  // 7番
+  {
+    id: 7,
+    name: "T’sキッチン",
+    description: "かき氷、イチゴ飴、光るドリンクボトル",
+    alcohol: "有り",
+    image: "exhibitor-07.png"
+  },
+
+  // 8番
+  {
+    id: 8,
+    name: "飛騨九兵衛",
+    description: "かき氷、チョコバナナ",
+    alcohol: "有り",
+    image: "exhibitor-08.jpg"
+  },
+
+  // 9番
+  {
+    id: 9,
+    name: "カラオケ酒場 じゅん婆",
+    description: "きゅうりの一本漬け",
+    alcohol: "有り",
+    image: "exhibitor-09.jpg"
+  },
+
+  // 10番
+  {
+    id: 10,
+    name: "ぱりぱりんご",
+    description: "りんご飴、ラムネ、水、ドリンク",
+    alcohol: "無し",
+    image: "exhibitor-10.jpg"
+  },
+
+  // 11番
+  {
+    id: 11,
+    name: "Ninotti",
+    description: "ジェラート、ドリンク",
+    alcohol: "有り",
+    image: "exhibitor-11.jpg"
+  },
+
+  // 12番
+  {
+    id: 12,
+    name: "いつめしplus",
+    description: "・ホットドッグ ・フランクフルト フライドポテト ・ドリンク かき氷 ・冷やしパイン",
+    alcohol: "無し",
+    image: "exhibitor-12.png"
+  },
+
+  // 13番
+  {
+    id: 13,
+    name: "しぇるぶーる",
+    description: "タコライス等、タコス、ポテトフライ、 揚げ物、いちご削り",
+    alcohol: "有り",
+    image: "exhibitor-13.png"
+  },
+
+  // 14番
+  {
+    id: 14,
+    name: "Koke Kokio",
+    description: "韓国料理、ソーセージチーズかけ ドリンク",
+    alcohol: "有り",
+    image: "exhibitor-14.png"
+  },
+
+  // 15番
+  {
+    id: 15,
+    name: "SMOKO",
+    description: "そば粉のガレット、ドリンク",
+    alcohol: "無し",
+    image: "exhibitor-15.png"
+  },
+
+  // 16番
+  {
+    id: 16,
+    name: "HOUEI COFFEE",
+    description: "コーヒー飲料",
+    alcohol: "無し",
+    image: "exhibitor-16.jpg"
+  },
+
+  // 17番
+  {
+    id: 17,
+    name: "ガキゲンキッチンカー",
+    description: "肉巻きおにぎり、チキンステーキ、 イカ串、牛カルビ串、かき氷",
+    alcohol: "有り",
+    image: "exhibitor-17.png"
+  },
+
+  // 18番
+  {
+    id: 18,
+    name: "まんまるキッチン",
+    description: "そば飯、明石焼き、フルーツジュース、 各種ドリンク、",
+    alcohol: "有り",
+    image: "exhibitor-18.png"
+  },
+
+  // 19番
+  {
+    id: 19,
+    name: "くまさんのカステラ",
+    description: "くまさんのカステラ、かき氷",
+    alcohol: "無し",
+    image: "exhibitor-19.png"
+  },
+
+  // 20番
+  {
+    id: 20,
+    name: "ゴールドキッチン",
+    description: "ケバブ",
+    alcohol: "無し",
+    image: "exhibitor-20.png"
+  },
+
+  // 21番
+  {
+    id: 21,
+    name: "crepe shop sunny's",
+    description: "クレープ・アメリカンポテト ソフトドリンク・かき氷・アルコール 肉巻きおにぎり",
+    alcohol: "有り",
+    image: "exhibitor-21.png"
+  },
+
+  // 22番
+  {
+    id: 22,
+    name: "旅するKitchen car岳",
+    description: "串焼き、スムージー、焼きそば、フライドポテト、唐揚げ",
+    alcohol: "有りア",
+    image: "exhibitor-22.png"
+  },
+
+  // 23番
+  {
+    id: 23,
+    name: "uis_kitchen",
+    description: "串焼き、焼きそば、フライドポテト、ドリンク",
+    alcohol: "有り",
+    image: "exhibitor-23.png"
+  },
+
+  // 24番
+  {
+    id: 24,
+    name: "植物ショクブツ",
+    description: "",
+    alcohol: "無し",
+    image: "exhibitor-24.png"
+  }
+
+];
